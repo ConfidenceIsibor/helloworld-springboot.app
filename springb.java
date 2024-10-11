ec55f122737f1a4fff56e209fd1bcae16db94842
ConfidenceIsibor <156429459+ConfidenceIsibor@users.noreply.github.com>
2024-10-11 15:01:24 +0100

    Add files via upload

---
+++ springb.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print Hello World
+        System.out.println("Hello, World!");
+
+        // Simple loop that counts from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
